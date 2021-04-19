5baa9e17d1ac4b971fd0811b105442217cd6d97e
Talya Gabso <talyagabso@gmail.com>
2021-04-19 17:07:06 +0300

    .

--- 11.1-express_with_react/bank-app/src/App.js
+++ 11.1-express_with_react/bank-app/src/App.js
@@ -1,35 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-import axios from 'axios';
-import './App.css'
+
 function App() {
+	const [data, setData] = useState([]);
 
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      let {data} = await axios.get('http://localhost:8000/api/bank/users');
-      console.log(data);
-      setData(data.users)
-    }
-
-    getData();
-  }, [])
-  return (
-    <div className="App">
-      <h2>Bank Users</h2>
-      {data.map((user, index) => {
-        return (
-          <div key={index+1} className="users-container">
-            <h3>user: {index+1}</h3>
-            <p>Id: {user.id}</p>
-            <p>cash: {user.cash}</p>
-            <p>credit: {user.credit}</p>
-          </div>
-        )
-      })}
-    </div>
-  );
+	useEffect(() => {
+		const getData = async () => {
+			const response = await axios.get("http://localhost:8000/api/bank/users");
+			setData(response.data.users);
+		};
+		getData();
+	}, []);
+	return (
+		<div className="App">
+				<h2>Bank Users</h2>
+				{data.map((user, index) => {
+					return (
+						<div key={index + 1} className="users-container">
+							<h3>user: {index + 1}</h3>
+							<p>Id: {user.id}</p>
+							<p>cash: {user.cash}</p>
+							<p>credit: {user.credit}</p>
+						</div>
+					);
+				})}
+		</div>
+	);
 }
-
 export default App;
